ff40ab0c7b62a38d926cfc49ac7005d8e189be84
Jean-Francois Arbour <jf.arbour@gmail.com>
2022-05-01 23:26:58 -0400

    Cleaned up unneeded methods

--- spring-challenge-2022/types.h
+++ spring-challenge-2022/types.h
@@ -49,13 +49,6 @@ enum class Offset : uint32_t
     NORTH_EAST = NORTH | EAST,
     NORTH_WEST = NORTH | WEST
 };
-enum class Corner : uint32_t
-{
-    NORTH_WEST = encode(0, 0),
-    NORTH_EAST = encode(WIDTH - 1, 0),
-    SOUTH_EAST = encode(WIDTH - 1, HEIGHT - 1),
-    SOUTH_WEST = encode(0, HEIGHT - 1)
-};
 
 inline auto constexpr get_x(Offset off) -> int
 {
@@ -74,89 +67,31 @@ inline auto constexpr Horizontal(Offset off) -> Offset
     return Offset(encode(get_x(off), 0));
 }
 
-struct Direction
-{
-    double angle{ 0.0 };
-    double normalizer{ -M_PIf64 };
-
-    constexpr Direction() = default;
-
-    explicit constexpr Direction(double angle_, double normalizer_ = -M_PIf64)
-            : angle{ angle_ }
-            , normalizer{ normalizer_ }
-    {}
-
-    /** Normalize the angle between (theta, theta + 2*PI] */
-    static constexpr auto normalized(double angle_, const double theta) -> double
-    {
-        // (angle - theta) = new_angle + 2*pi*N with new_angle normalized
-        double wrapped_angle = (angle_ - theta) * 0.5 * M_1_PI;  // N + new_angle / (2pi)
-
-        // N
-        int n_full_turns = std::floor(wrapped_angle);  // Round down so that real-part is positive (between 0 and 1)
-
-        double new_angle = theta + 2.0 * M_PI * (wrapped_angle - n_full_turns);
-
-        assert(new_angle > theta && new_angle < theta + 2.0 * M_PI + 0.000001);
-        return new_angle;
-    }
-
-    static constexpr auto offset(const Direction& dir) -> Offset
-    {
-        constexpr std::array<Offset, 8> offsets_thresholds{ Offset::WEST,  Offset::SOUTH_WEST,
-                                                            Offset::SOUTH, Offset::SOUTH_EAST,
-                                                            Offset::EAST,  Offset::NORTH_EAST,
-                                                            Offset::NORTH, Offset::NORTH_WEST };
-        const double a = normalized(dir.angle, -M_PI);
-        for (int i = 0; i < 8; ++i) {
-            // if (a < -M_PI + (2 * i + 1) * M_PI / 8) {
-            //     return offsets_thresholds[i];
-            // }
-            // Equivalent to -pi + pi/8 + i*(pi/4)
-            if (a < (0.25*i - 0.875) * M_PI) {
-                return offsets_thresholds[i];
-            }
-        }
-        return Offset::WEST;
-    }
-};
-
 struct Point
 {
     int x;
     int y;
 
-    constexpr Point()
-            : x{ WIDTH }
-            , y{ HEIGHT - 1 }
-    {}
-    constexpr Point(int x_, int y_)
-            : x{ x_ }
-            , y{ y_ }
-    {}
-    explicit constexpr Point(Offset off)
-            : x{ get_x(off) }
-            , y{ get_y(off) }
-    {}
-
     constexpr auto operator!=(const Point& other) const noexcept -> bool
     {
         return this->x != other.x || this->y != other.y;
     }
-    constexpr auto operator==(const Point& other) const noexcept -> bool
+    template<typename Other>
+    constexpr auto operator==(const Other& other) const noexcept -> bool
     {
         return this->x == other.x && this->y == other.y;
     }
-
-    [[nodiscard]] inline auto constexpr direction_of(const Point& p, double normalization = -M_PI) const
-      -> Direction
+    template<typename Offset>
+    inline auto constexpr operator+=(const Offset& offset) -> Point&
     {
-        auto dx = static_cast<double>(p.x - this->x);
-        auto dy = static_cast<double>(p.y - this->y);
-        auto angle = Direction::normalized(std::atan2(-dy, dx), normalization);  // -dy because y-axis is "pointing downwards"
-        return Direction{ angle };
+        this->x += offset.x;
+        this->y += offset.y;
+        return *this;
     }
-    [[nodiscard]] constexpr auto encoded(const Point& p) const noexcept -> uint32_t { return encode(p.x, p.y); }
+    [[nodiscard]] constexpr auto encoded(const Point& p) const noexcept -> uint32_t
+    {
+        return encode(p.x, p.y);
+    }
 };
 
 inline auto constexpr
@@ -165,33 +100,44 @@ operator+(const Point& a, const Point& b) -> Point
     return { a.x + b.x, a.y + b.y };
 }
 inline auto constexpr
-operator+(const Point& p, Offset off) -> Point
-{
-    return p + Point{ off };
-}
-inline auto constexpr
 operator-(const Point& p) -> Point
 {
     return Point{ -p.x, -p.y };
 }
-inline auto constexpr directed_floor(double x_, double y_, Direction dir) -> Point
+
+inline auto constexpr
+operator*(const Point& p, double d) -> Point
 {
-    const auto off = Direction::offset(dir);
-    Point p{};
-
-    if (Horizontal(off) == Offset::EAST) {
-        p.x = std::floor(x_);
+    Point pd{};
+    double px = p.x * d;
+    double py = p.y * d;
+    if (px > WIDTH / 2.0) {
+        if (d > 1.0) {
+            pd.x = std::ceil(px);
+        } else {
+            pd.x = std::floor(px);
+        }
     } else {
-        p.x = std::ceil(x_);
+        if (d > 1.0) {
+            pd.x = std::floor(px);
+        } else {
+            pd.x = std::ceil(px);
+        }
     }
-
-    if (Vertical(off) == Offset::SOUTH) {
-        p.y = std::floor(y_);
+    if (py > HEIGHT / 2.0) {
+        if (d > 1.0) {
+            pd.y = std::ceil(py);
+        } else {
+            pd.y = std::floor(py);
+        }
     } else {
-        p.y = std::ceil(y_);
+        if (d > 1.0) {
+            pd.y = std::floor(py);
+        } else {
+            pd.y = std::ceil(py);
+        }
     }
-
-    return p;
+    return pd;
 }
 
 inline auto constexpr dot(const Point& a, const Point& b) -> double
@@ -206,32 +152,70 @@ inline auto constexpr distance(const Point& a, const Point& b) -> double
 {
     return std::sqrt(distance2(a, b));
 }
+inline constexpr auto
+norm(const Point& p) -> double
+{
+    constexpr Point O{ 0, 0 };
+    return distance(p, O);
+}
 
+inline constexpr auto
+manhattan_norm(const Point& p) -> int
+{
+    return std::abs(p.x) + std::abs(p.y);
+}
+
+inline constexpr auto
+manhattan_dist(const Point& a, const Point& b) -> int
+{
+    return std::abs(a.x - b.x) + std::abs(a.y - b.y);
+}
 
 struct Vector
 {
     Point p;
-    Direction dir;
-    double norm;
+    double x;
+    double y;
+    [[nodiscard]] auto target() const noexcept -> Point {
+        Point ret;
+        if (p.x + x > WIDTH / 2.0) {
+            ret.x = std::ceil(p.x + x);
+        } else {
+            ret.x = std::floor(p.x + x);
+        }
+        if (p.y + y > HEIGHT / 2.0) {
+            ret.y = std::ceil(p.y + y);
+        } else {
+            ret.y = std::floor(p.y + y);
+        }
+        return ret;
+    }
 };
+
 inline auto constexpr
 operator*(const Vector& v, double s) -> Vector
 {
-    return { v.p, v.dir, v.norm * s };
+    return { v.p, v.x * s, v.y * s };
 }
+
 inline auto constexpr
 operator-(const Point& a, const Point& b) -> Vector
 {
-    return { b, b.direction_of(a), distance(a, b) };
-}
-inline constexpr auto
-Flow(const Vector& v) -> Vector
-{
-    double x = v.p.x + v.norm * std::cos(v.dir.angle);
-    double y = v.p.y + v.norm * std::sin(v.dir.angle);
-    return { directed_floor(x, y, v.dir), v.dir, v.norm };
+    return { b, double(a.x - b.x), double(a.y - b.y) };
 }
 
+inline constexpr auto
+Unit(const Point& p) -> Vector
+{
+    double d = norm(p);
+    return { p, p.x/d, p.y/d };
+}
+constexpr const Point Point_None{ WIDTH, HEIGHT - 1 };
+
+struct Segment {
+    Point start;
+    Point end;
+};
 
 } // namespace spring
 
